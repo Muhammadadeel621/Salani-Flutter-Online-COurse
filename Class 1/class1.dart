c2fbc8ae03de9d6d86fa4391bc837bef591ca2bc
tooba889 <toobatooba891@gmail.com>
2023-07-02 12:55:55 +0500

    class1 and class2 Practice

---
+++ Class 1/class1.dart
@@ -0,0 +1,38 @@
+void main() {
+  // int age = 15 ;   // int
+  // print(age);       // variable
+
+  // these are the Comments.
+  // var age = 35;   // how to create a variable
+  // print(age);
+
+  // print("Hello World");
+  // print("12");
+  // print(12);
+  // print("2+2");
+  // print(2+2);
+
+  // String name = "Muhammad Adeel";
+  // int age = 30;
+  // double height = 5.9;
+  // num weight = 80;
+  // print(name);
+  // print(age);
+  // print(height);
+  // print(weight);
+
+  // int age = 30;
+  // int age = 12;
+  // two variables of same name can't be assigned to a single file but can be overwritten
+
+  // int age = 30;
+  // age = 12;
+
+  // print(age);
+  // var age = 35;  how to create a variable
+  // through error
+
+  // int age1 = 13;
+  // int age2 = age1;
+  // print(age2);
+}
---
+++ class 2/class2.dart
@@ -0,0 +1,92 @@
+void main() {
+  //   bool abc = 12>9;
+//   print(abc);
+
+//   Arithmetic   (+ , - , * , / , ~/ , % , ++ , --)
+
+//   int num1 = 34;
+//   int num2 = 56;
+//   print(num1+num2);
+//   print(num1-num2);
+//   print(num1*num2);
+//   print(num1/num2);
+//   print(num1~/num2);
+//   print(num1%num2);
+//   print(num1++);
+//   print(num1--);
+
+//   Equality and Relational  (> , < , >= , <= , ++ , == , !=  )
+
+//   int age = 18;
+//   bool result = age >= 18;
+//   print(result);
+
+//     int age = 19;
+//   bool result = age >= 18;
+//   print(result);
+
+//   int age = 17;
+//   bool result = age <= 18;
+//   print(result);
+
+//     int age = 18;
+//   bool result = age != 18;
+//   print(result);
+
+//   logical (AND &&  , OR || , NOT !)
+
+//   String email = "b19102059@gmail.com";
+//   String password = "Adeelrao";
+//   bool condition = email == "b19102059@gmail.com" && password == "Adeelrao" ;
+//   print (condition);
+
+//   String email = "b19102059@gmail.com";
+//   String password = "Adeerao";
+//   bool condition = email == "b19102059@gmail.com" || password == "Adeelrao" ;
+//   print (condition);
+
+//   print (!condition);
+
+//   int abc = 34;
+//   int a = 21;
+//   int q = 12;
+//   int k = 8 ;
+//   int i = a-q ;   // 9
+
+//   bool w = abc > a && !(a<abc);
+//          34 > 21 && !(21<34)   --> T && !(T) --> T && F --> F
+
+//   bool e  = !w || q !=k ;
+//          ! F || 12!=8   -->  T || T --> T
+
+//   bool o = e && i > 4 ;    // T && T --> T
+
+//   print(!(!o || e));       // !(!T || T) --> !(F || T) --> !(T) --> False
+
+//        if-else strcuture
+//   if(condition true then if , otherwise false){
+
+//   }
+//   e;se{
+
+//   }
+
+//   int num1 = 12;
+//   int num2 = 10;
+//   if (num1 > num2){
+//     print("num1 is greater than num2");
+//   }else {
+//     print("num2 is greater than num1");
+//   }
+
+//   int num1 = 12;
+//   int num2 = 12;
+//   if (num1 > num2){
+//     print("num1 is greater than num2");
+//   } else if(num1 == num2){
+//     print("num1 is equal to num2");
+//   } else {
+//     print("num2 is greater than num1");
+//   }
+//   print("yahn jo bh print kerogy , chlyga ..!!");
+}
